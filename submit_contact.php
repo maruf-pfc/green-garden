ee614caf2c5b066eed278e5e6aa4f82ec17f318b
Md. Maruf Sarker <mdmarufsarker.mms@gmail.com>
2024-10-23 06:27:24 +0600

    Update submit_contact.php

--- submit_contact.php
+++ submit_contact.php
@@ -38,14 +38,14 @@ try {
     $mail->isSMTP(); //Send using SMTP
     $mail->Host = "smtp.gmail.com"; //Set the SMTP server to send through
     $mail->SMTPAuth = true; //Enable SMTP authentication
-    $mail->Username = "marufsarkercse2022@gmail.com"; //SMTP username
-    $mail->Password = "mradurptsatagwkp"; //SMTP password
+    $mail->Username = "example@gmail.com"; //SMTP username
+    $mail->Password = "**** **** **** ****"; //SMTP password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS; //Enable implicit TLS encryption
     $mail->Port = 465; //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
     //Recipients
-    $mail->setFrom("marufsarkercse2022@gmail.com", "Green Garden");
-    $mail->addAddress("marufsarkercse2022@gmail.com", "$name");
+    $mail->setFrom("example@gmail.com", "Green Garden");
+    $mail->addAddress("example@gmail.com", "$name");
 
     //Content
     $mail->isHTML(true); //Set email format to HTML
